fb1a236fc56a0ea5b2d14240d1243a20ee0d05a5
Hugo Morosini <hugo.morosini@gmail.com>
2021-04-08 19:18:37 +0200

    working account selection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import "../styles/globals.css"
 
 import 'modern-normalize'
 
--- pages/app/dapp-browser.tsx
+++ pages/app/dapp-browser.tsx
@@ -1,38 +1,91 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {useState, useEffect, useRef, useCallback, useMemo} from "react";
 import styled from "styled-components";
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import Select from 'react-select'
 
 const AppLoaderPageContainer = styled.div`
-  
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const DappBrowserTopBar = styled.div`
+  box-sizing: border-box;
+  background-color: black;
+  color: white;
+  padding: 8px 16px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const DappIframe = styled.iframe`
     width: 100%;
-    height: 600px;
+    flex: 1;
+    border: 0;
 `
 
 const PROJECT_ID = "a1664f14bbf54437acd24a79a600e3cc"
 
-const DappBrowser = () => {
-    const router = useRouter()
-    const iframeRef = useRef<HTMLIFrameElement>(null);
-    const [mounted, setMounted] = useState(false);
+const options = [
+    { value: '0x407d73d8a49eeb85d32cf465507dd71d507100c1', label: 'Account 1 (0x407d73d8a49eeb85d32cf465507dd71d507100c1)' },
+    { value: '0xcad38ab8336682b6e084ade61c546183c5ef7582', label: 'Savings Account (0xcad38ab8336682b6e084ade61c546183c5ef7582)' },
+    { value: '0x6bf346da89e93ade5be864b483182eb4190246c4', label: 'Account 3 (0x6bf346da89e93ade5be864b483182eb4190246c4)' }
+];
 
-    const { url } = router.query
-    console.log({ url })
+const styles = {
+    control: (provided: any) => ({
+        ...provided,
+        width: 400,
+        backgroundColor: "black",
+    }),
+    singleValue: (provided: any) => ({
+        ...provided,
+        color: "white",
+        fontSize: 12
+    }),
+    indicatorsContainer: (provided: any) => ({
+        ...provided,
+        color: "white",
+    }),
+    menu: (provided: any) => ({
+        ...provided,
+        backgroundColor: "black",
+    }),
+    option: (provided: any) => ({
+        ...provided,
+        fontSize: 12,
+        backgroundColor: "rgba(0, 0, 0, 0.1)"
+    })
+}
 
-    useEffect(() => {
-        if (!url) {
-            return;
+const DappBrowser = ({ url }: { url: string }) => {
+    const iframeRef = useRef<HTMLIFrameElement | null>(null);
+    const [option, setOption] = useState(options[0]);
+    const dappURL = useMemo(() => new URL(url), [url]);
+
+    const handleAccountChange = useCallback(value => {
+        if (iframeRef.current && iframeRef.current.contentWindow) {
+            console.log("sending new account to dapp")
+            iframeRef.current.contentWindow.postMessage({
+                "jsonrpc": "2.0",
+                "method": "accountsChanged",
+                "params": [
+                    [value.value]
+                ]
+            }, dappURL.origin);
         }
 
-        const dappURL = new URL(String(url));
-        const ws = new WebSocket(`wss://ropsten.infura.io/ws/v3/${PROJECT_ID}`);
+        setOption(value);
+    }, [setOption])
 
+    useEffect(() => {
         if (iframeRef.current) {
             iframeRef.current.src = dappURL.toString();
         }
 
+        const ws = new WebSocket(`wss://ropsten.infura.io/ws/v3/${PROJECT_ID}`);
         ws.onopen = () => {
             console.log("connected to Infura node")
         }
@@ -45,9 +98,8 @@ const DappBrowser = () => {
             }
         }
 
-
-        window.addEventListener("message", function(event) {
-            if (event.origin === dappURL.origin && event.source && event.source instanceof Window) {
+        window.addEventListener("message", function(event: MessageEvent) {
+            if (event.origin === dappURL.origin && event.source && !(event.source instanceof MessagePort) && !(event.source instanceof ServiceWorker)) {
                 const data = event.data;
                 console.log("from dapp: ", data)
                 switch (data.method) {
@@ -55,7 +107,7 @@ const DappBrowser = () => {
                         event.source.postMessage({
                             "id": data.id,
                             "jsonrpc": "2.0",
-                            "result": ["0x407d73d8a49eeb85d32cf465507dd71d507100c1"]
+                            "result": [option.value]
                         }, event.origin);
                         break;
                     }
@@ -63,7 +115,7 @@ const DappBrowser = () => {
                         event.source.postMessage({
                             "id": data.id,
                             "jsonrpc": "2.0",
-                            "result": ["0x407d73d8a49eeb85d32cf465507dd71d507100c1"]
+                            "result": [option.value]
                         }, event.origin);
                         break;
                     }
@@ -83,19 +135,43 @@ const DappBrowser = () => {
                     }
                 }
             }
-        })
-        setMounted(true)
+        });
     }, [url])
 
-    if (!mounted) {
-        return null;
-    }
-
+    console.log(option)
     return (
         <AppLoaderPageContainer>
+            <DappBrowserTopBar>
+                Ledger DAPP Browser test tool
+                <Select
+                    options={options}
+                    styles={styles}
+                    onChange={handleAccountChange}
+                    value={option}
+                />
+            </DappBrowserTopBar>
             <DappIframe ref={iframeRef} />
         </AppLoaderPageContainer>
     );
 };
 
-export default DappBrowser;
+function DappBrowserPage() {
+    const [mounted, setMounted] = useState(false);
+    const router = useRouter();
+
+    const { url } = router.query;
+
+    useEffect(() => {
+        setMounted(true);
+        return () => setMounted(false);
+    }, []);
+
+    if (mounted) {
+        return (
+            !!url ? <DappBrowser url={String(url)} /> : null
+        )
+    }
+    return null;
+}
+
+export default DappBrowserPage;
