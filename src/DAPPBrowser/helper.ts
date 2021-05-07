ff0e0a59c85fb5e90de690833b434d321e5b1ddd
Hugo Morosini <hugo.morosini@gmail.com>
2021-05-08 00:11:18 +0200

    improved dapp reply

--- src/DAPPBrowser/helper.ts
+++ src/DAPPBrowser/helper.ts
@@ -4,7 +4,7 @@ import BN from "bn.js";
 export function convertEthToLiveTX(ethTX: any): EthereumTransaction {
     return {
         family: "ethereum",
-        amount: ethTX.amount !== undefined ? new BN(ethTX.value.replace("0x", ""), 16).toString(10) : undefined,
+        amount: ethTX.amount !== undefined ? new BN(ethTX.value.replace("0x", ""), 16).toString(10) : "0",
         recipient: ethTX.to,
         gasPrice: ethTX.gasPrice !== undefined ? new BN(ethTX.gasPrice.replace("0x", ""), 16).toString(10) : undefined,
         gasLimit: ethTX.gas !== undefined ? new BN(ethTX.gas.replace("0x", ""), 16).toString(10) : undefined,
