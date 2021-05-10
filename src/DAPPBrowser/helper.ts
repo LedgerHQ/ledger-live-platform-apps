189cac1c833cf9a73973992e7bff8b261602ec88
Hugo Morosini <hugo.morosini@gmail.com>
2021-05-10 12:17:24 +0200

    fixed amount conversion bug eth tx -> platform tx

--- src/DAPPBrowser/helper.ts
+++ src/DAPPBrowser/helper.ts
@@ -4,7 +4,7 @@ import { BigNumber } from "bignumber.js";
 export function convertEthToLiveTX(ethTX: any): EthereumTransaction {
     return {
         family: "ethereum",
-        amount: ethTX.amount !== undefined ? new BigNumber(ethTX.value.replace("0x", ""), 16) : new BigNumber(0),
+        amount: ethTX.value !== undefined ? new BigNumber(ethTX.value.replace("0x", ""), 16) : new BigNumber(0),
         recipient: ethTX.to,
         gasPrice: ethTX.gasPrice !== undefined ? new BigNumber(ethTX.gasPrice.replace("0x", ""), 16) : undefined,
         gasLimit: ethTX.gas !== undefined ? new BigNumber(ethTX.gas.replace("0x", ""), 16) : undefined,
