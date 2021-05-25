0148ad59dbf57558cda1e03de896aa014e9d6d48
Hugo Morosini <hugo.morosini@gmail.com>
2021-05-25 10:43:45 +0200

    added Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,9 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Ledger Live App Manager</title>
+                <title>Ledger Platform Apps</title>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
             </Head>
             <ThemeProvider theme={theme}>
                 <GlobalStyle />
--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
     color: ${p => p.theme.colors.text};
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
   
