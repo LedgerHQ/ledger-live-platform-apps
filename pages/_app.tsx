f356d003afb70ca6f99df9ac3f63bf30bd732044
Rémi Jarasson (Ext) <remi.jarasson-ext@ledger.fr>
2021-06-03 17:54:18 +0200

    fix(Document): disable zoom completely

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
                 <title>Ledger Platform Apps</title>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
