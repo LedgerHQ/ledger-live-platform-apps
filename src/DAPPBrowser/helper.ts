345f3e112993b17921b57f5cc88db88a6f8b6240
Rémi Jarasson (Ext) <remi.jarasson-ext@ledger.fr>
2021-06-03 15:32:05 +0200

    fix(DappBrowser): encode recipient address to EIP55 - LL-5687

--- src/DAPPBrowser/helper.ts
+++ src/DAPPBrowser/helper.ts
@@ -1,11 +1,13 @@
-import {EthereumTransaction} from "../../lib/types";
 import { BigNumber } from "bignumber.js";
+import eip55 from "eip55";
+
+import { EthereumTransaction } from "../../lib/types";
 
 export function convertEthToLiveTX(ethTX: any): EthereumTransaction {
     return {
         family: "ethereum",
         amount: ethTX.value !== undefined ? new BigNumber(ethTX.value.replace("0x", ""), 16) : new BigNumber(0),
-        recipient: ethTX.to,
+        recipient: eip55.encode(ethTX.to),
         gasPrice: ethTX.gasPrice !== undefined ? new BigNumber(ethTX.gasPrice.replace("0x", ""), 16) : undefined,
         gasLimit: ethTX.gas !== undefined ? new BigNumber(ethTX.gas.replace("0x", ""), 16) : undefined,
         data: ethTX.data ? Buffer.from(ethTX.data.replace("0x", ""), "hex") : undefined,
